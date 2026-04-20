fd9916a11dc3c621e6ff7d0d8736a6b249fbfe0f
Devansh Sharma <121448392+deku0002@users.noreply.github.com>
2026-04-20 18:03:09 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,4 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:PASSWORD@database-2.cdecyo464bcu.ap-south-1.rds.amazonaws.com:3306/lms'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
